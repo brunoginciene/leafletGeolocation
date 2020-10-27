595e47e14278549548e468b6b398a98aed194e9f
Bruno Rodrigues Ginciene <brunoginciene@hotmail.com>
2020-10-27 19:02:19 +0000

    setting up

---
+++ public/geolocation.js
@@ -0,0 +1,14 @@
+if ('geolocation' in navigator) {
+    console.log('geolocation available')
+    navigator.geolocation.getCurrentPosition(position => {
+        let lat = position.coords.latitude
+        let long = position.coords.longitude
+        let accuracy = position.coords.accuracy
+
+        document.getElementById('lat').textContent = lat
+        document.getElementById('long').textContent = long
+        document.getElementById('accuracy').textContent = accuracy
+    })
+} else {
+    console.log('geolocation not available')
+}
